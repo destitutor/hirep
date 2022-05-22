e6caf86d4497a1bb85d5a337e5ac52dd6738e98b
Sanghyeon Lee <layer6ai@gmail.com>
2022-05-22 21:44:09 +0900

    my first commit

---
+++ src/main/java/net/hexabrain/hireo/HireoApplication.java
@@ -0,0 +1,13 @@
+package net.hexabrain.hireo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HireoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HireoApplication.class, args);
+    }
+
+}
