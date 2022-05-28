288afdf36aa0c8c7fb1869a6fe457d78bb3bbe40
Sanghyeon Lee <layer6ai@gmail.com>
2022-05-29 08:12:10 +0900

    Add Signup page

---
+++ src/main/java/net/hexabrain/hireo/domain/Freelancer.java
@@ -0,0 +1,17 @@
+package net.hexabrain.hireo.domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity @Getter
+@DiscriminatorValue("FRE")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Freelancer extends Account {
+    public Freelancer(String email, String password, AccountType type) {
+        super(email, password, type);
+    }
+}
