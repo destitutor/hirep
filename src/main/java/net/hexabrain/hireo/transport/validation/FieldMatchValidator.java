83a6a88e1558da97f57111f1bde0190e24876862
Sanghyeon Lee <layer6ai@gmail.com>
2022-05-29 08:17:50 +0900

    Add an annotation used for field matches

---
+++ src/main/java/net/hexabrain/hireo/transport/validation/FieldMatchValidator.java
@@ -0,0 +1,35 @@
+package net.hexabrain.hireo.transport.validation;
+
+import net.hexabrain.hireo.transport.validation.constraints.FieldMatch;
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.PropertyAccessorFactory;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.Objects;
+
+public class FieldMatchValidator implements ConstraintValidator<FieldMatch, Object> {
+    private String firstFieldName;
+    private String secondFieldName;
+
+    @Override
+    public void initialize(FieldMatch constraintAnnotation) {
+        this.firstFieldName = constraintAnnotation.first();
+        this.secondFieldName = constraintAnnotation.second();
+    }
+
+    @Override
+    public boolean isValid(Object value, ConstraintValidatorContext context) {
+        final BeanWrapper wrapper = PropertyAccessorFactory.forBeanPropertyAccess(value);
+
+        try {
+            final Object firstValue = wrapper.getPropertyValue(firstFieldName);
+            final Object secondValue = wrapper.getPropertyValue(secondFieldName);
+
+            return Objects.equals(firstValue, secondValue);
+        } catch (BeansException e) {
+            return false;
+        }
+    }
+}
---
+++ src/main/java/net/hexabrain/hireo/transport/validation/constraints/FieldMatch.java
@@ -0,0 +1,42 @@
+package net.hexabrain.hireo.transport.validation.constraints;
+
+import net.hexabrain.hireo.transport.validation.FieldMatchValidator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Target({TYPE, ANNOTATION_TYPE})
+@Retention(RUNTIME)
+@Constraint(validatedBy = FieldMatchValidator.class)
+public @interface FieldMatch {
+
+    String message() default "{net.hexabrain.hireo.transport.validation.constraints.FieldMatch.message}";
+
+    Class<?>[] groups() default { };
+
+    Class<? extends Payload>[] payload() default { };
+
+    /**
+     * @return 매치되어야 하는 첫 번째 필드 이름
+     */
+    String first();
+
+    /**
+     * @return 매치되어야 하는 두 번째 필드 이름
+     */
+    String second();
+
+    @Target({ TYPE, ANNOTATION_TYPE })
+    @Retention(RUNTIME)
+    @Documented
+    @interface List {
+        FieldMatch[] value();
+    }
+}
