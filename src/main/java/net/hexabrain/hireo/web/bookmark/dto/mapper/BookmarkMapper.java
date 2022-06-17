7031f4b386fdb6e2e3ba28b4af48585ec1e6c699
Sanghyeon Lee <layer6ai@gmail.com>
2022-06-17 20:27:43 +0900

    rename: 북마크 DTO의 이름 변경

--- src/main/java/net/hexabrain/hireo/web/bookmark/dto/mapper/BookmarkMapper.java
+++ src/main/java/net/hexabrain/hireo/web/bookmark/dto/mapper/BookmarkMapper.java
@@ -4,13 +4,13 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 import net.hexabrain.hireo.web.bookmark.domain.Bookmark;
-import net.hexabrain.hireo.web.bookmark.dto.BookmarkedCompanyResponse;
+import net.hexabrain.hireo.web.bookmark.dto.BookmarkDto;
 import net.hexabrain.hireo.web.common.mapper.BaseMapper;
-import net.hexabrain.hireo.web.company.dto.mapper.CompanyResponseMapper;
 
-@Mapper(componentModel = "spring", uses = { CompanyResponseMapper.class })
-public interface BookmarkMapper extends BaseMapper<BookmarkedCompanyResponse, Bookmark> {
+@Mapper(componentModel = "spring")
+public interface BookmarkMapper extends BaseMapper<BookmarkDto, Bookmark> {
 
-	@Mapping(source = "createdDate", target="bookmarkedAt")
-	BookmarkedCompanyResponse toDto(Bookmark bookmark);
+	@Mapping(source = "company.id", target="companyId")
+	@Mapping(source = "job.id", target="jobId")
+	BookmarkDto toDto(Bookmark bookmark);
 }
